9359094335135a126f419c6d9b71cbd2c2d0e629
Bojana Skarich <bojana.skarich@gmail.com>
2016-11-26 16:15:28 -0800

    Add all method to Racer class.

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -10,6 +10,21 @@ class Racer
 	  self.mongo_client['racers']
 	end
 
+	# Create a class method called "all" that must: 
+	# Accept an optional prototype, optional sort, optional skip, and optional limit. 
+	# Is to “match all” – which means you must provide it a document that matches all records. The default for sort must be by number ascending. The default for skip must be 0 and the default for limit must be nil.
+	# Find all racers that match the given prototype.
+	# Sort them by the given hash criteria.
+	# Skip the specified number of documents.
+	# Limit the number of documents returned if limit is specified.
+	# Return the result.
+	def self.all(prototype={}, sort={}, skip=0, limit=nil)
+ 	  result = collection.find(prototype).sort(sort).skip(skip)
+ 	    if limit then result.limit(limit)
+ 	    else result 
+ 	    end
+	end
+
 
 
 end
