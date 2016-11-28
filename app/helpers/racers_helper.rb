744574509b7c7485a064c380c7b1c1e8b6b4e143
Bojana Skarich <bojana.skarich@gmail.com>
2016-11-27 17:31:52 -0800

    Update views with toRacer helper method

--- app/helpers/racers_helper.rb
+++ app/helpers/racers_helper.rb
@@ -1,2 +1,8 @@
 module RacersHelper
+  # Add an instance method to the generated RacersHelper class called toRacer. The new method must:
+	# Accept a single input argument.
+	# If the type of the input argument is a Racer, simply return the instance unmodified. Else attempt to instantiate a Racer from the input argument and return the result.
+	def toRacer(value)
+	  return value.is_a?(Racer) ? value : Racer.new(value)
+	end
 end
